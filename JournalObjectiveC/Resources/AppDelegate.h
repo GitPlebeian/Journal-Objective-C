b4519be4c5b393a4a4e3a3caa685fd218a87a1ae
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-19 11:32:45 -0600

    Initial commit

---
+++ JournalObjectiveC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalObjectiveC
+//
+//  Created by Jackson Tubbs on 8/19/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
